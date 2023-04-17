511b8b30b71269758b716fe3c231d34b7c1a672b
Joseph Ojoko <entewshi@gmail.com>
2023-04-17 13:21:24 +0100

    feat: move template for missing notification to a tiny component

---
+++ frontend/app/src/components/status/notifications/MissingKeyNotification.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { type I18nParam } from '@rotki/common/lib/messages';
+
+defineProps<{
+  params: I18nParam;
+}>();
+</script>
+
+<template>
+  <i18n :path="params.message" :plural="params.choice" tag="div">
+    <template #service>
+      {{ params.props.service }}
+    </template>
+    <template #location>
+      {{ params.props.location }}
+    </template>
+    <template #url>
+      <a :href="params.props.url" target="_blank">{{ params.props.url }}</a>
+    </template>
+  </i18n>
+</template>
